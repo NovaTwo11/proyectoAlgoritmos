42c08dc7c9767b136dbfaa9dec6c8faeb20a5197
HoldanLopez <holdann.lopezs@uqvirtual.edu.co>
2025-09-25 19:13:58 -0500

    Primer commit - Proyecto Algoritmos

---
+++ src/main/java/co/edu/uniquindio/proyectoAlgoritmos/ProyectoAlgoritmosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectoAlgoritmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAlgoritmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAlgoritmosApplication.class, args);
+	}
+
+}
